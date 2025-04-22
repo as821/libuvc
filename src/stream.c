d87fe64fe36f8331baa2339e7ec14c10675daea4
Andrew Stange <andrew@armstrong.ai>
2025-04-21 18:30:02 -0700

    keep max compression

--- src/stream.c
+++ src/stream.c
@@ -495,12 +495,12 @@ uvc_error_t uvc_get_stream_ctrl_format_size(
         UVC_DEBUG("claiming streaming interface %d", stream_if->bInterfaceNumber );
         uvc_claim_if(devh, ctrl->bInterfaceNumber);
         /* get the max values */
-        uvc_query_stream_ctrl( devh, ctrl, 1, UVC_GET_MIN);
-        printf("\n\nMIN SETTINGS: \n");
-        uvc_print_stream_ctrl(ctrl, stderr);
-        uint16_t min_kfr = ctrl->wKeyFrameRate;
-        uint16_t min_pfr = ctrl->wPFrameRate;
-        uint16_t min_comp_window = ctrl->wCompWindowSize;
+        // uvc_query_stream_ctrl( devh, ctrl, 1, UVC_GET_MIN);
+        // printf("\n\nMIN SETTINGS: \n");
+        // uvc_print_stream_ctrl(ctrl, stderr);
+        // uint16_t min_kfr = ctrl->wKeyFrameRate;
+        // uint16_t min_pfr = ctrl->wPFrameRate;
+        // uint16_t min_comp_window = ctrl->wCompWindowSize;
         
         // libuvc originally just used max values. default to max but overwrite as needed
         uvc_query_stream_ctrl( devh, ctrl, 1, UVC_GET_MAX);
@@ -509,9 +509,9 @@ uvc_error_t uvc_get_stream_ctrl_format_size(
 
         // set these values to min, keep wCompQuality at max
         // https://github.com/torvalds/linux/blob/master/drivers/media/usb/uvc/uvc_video.c#L468
-        ctrl->wKeyFrameRate = min_kfr;
-        ctrl->wPFrameRate = min_pfr;
-        ctrl->wCompWindowSize = min_comp_window;
+        // ctrl->wKeyFrameRate = min_kfr;
+        // ctrl->wPFrameRate = min_pfr;
+        // ctrl->wCompWindowSize = min_comp_window;
 
 
         printf("\n\nSTREAMING INTERFACE: %d (%u)\n", stream_if->bInterfaceNumber, ctrl->dwMaxPayloadTransferSize);
