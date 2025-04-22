0de7f7c8a84b5aa631f639f7996bb13054d545e3
Andrew Stange <andrew@armstrong.ai>
2025-04-22 12:20:18 -0700

    10fps

--- src/example.c
+++ src/example.c
@@ -193,7 +193,7 @@ int main(int argc, char **argv) {
         fps = 10000000 / frame_desc->dwDefaultFrameInterval;
       }
 
-      fps = 15;   // 4 @ 20fps is flaky :(
+      fps = 10;   // 4 @ 20fps is flaky :(
 
       printf("\nFirst format: (%4s) %dx%d %dfps\n", format_desc->fourccFormat, width, height, fps);
 
